9acbda87cc8d11ed99e9f485ac5e8966036c925f
Sounak Saha <88291739+sounak999@users.noreply.github.com>
2022-05-04 10:13:20 +0530

    Time: 27 ms (93.80%), Space: 26.5 MB (68.99%) - LeetHub

--- 581-shortest-unsorted-continuous-subarray/581-shortest-unsorted-continuous-subarray.cpp
+++ 581-shortest-unsorted-continuous-subarray/581-shortest-unsorted-continuous-subarray.cpp
@@ -1,22 +1,38 @@
 class Solution {
 public:
     int findUnsortedSubarray(vector<int>& nums) {
-        int i = 0, j = nums.size()-1;
-        vector<int> aux = nums;
-        sort(aux.begin(), aux.end());
+
+        int ans = 0, n = nums.size();  
         
-        if(nums == aux) 
-            return 0;
+        // Two pointers
+        int i = 0;
+        int j = n - 1;
+
+        int curr_min = INT_MAX; // from right we'll find the min element
+        int curr_max = INT_MIN; // from left we'll find the max element
         
-        while(nums[i] == aux[i] || nums[j] == aux[j])
-        {
-            if(nums[i] == aux[i])
-                i++;
-                
-            if(nums[j] == aux[j])
-                j--;
+        // Initialize end to -1, if we have all sorted in ascending order, then end-start+1 = -1+0+1 = 0 
+        // "end" for left, "start" for right
+        int start = 0, end = -1;
+        
+        while(j >= 0 && i < n){
+            // Check if the "num[start]" is greater than the minimum element so far
+            if(nums[i] >= curr_max) 
+                curr_max = nums[i];
+            else 
+                end = i; // if the curr_max isn't updating, we'll store that index in "end"
+
+            // Check if the "num[end]" is less than the maximum element so far
+            if(nums[j] <= curr_min) 
+                curr_min = nums[j];
+            else 
+                start = j; // if the curr_min isn't updating, we'll store that index in "start"
+
+            ++i;
+            --j;
         }
         
-        return j-i+1;
+        // defining the number of elements
+        return end - start + 1;
     }
 };
