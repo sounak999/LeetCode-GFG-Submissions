699e42195a91c3773a5c1404e556f8f326e413a3
Sounak Saha <88291739+sounak999@users.noreply.github.com>
2022-05-04 23:44:27 +0530

    Added solution - LeetHub

--- Lexicographically Previous Permutation - GFG/lexicographically-previous-permutation.cpp
+++ Lexicographically Previous Permutation - GFG/lexicographically-previous-permutation.cpp
@@ -8,11 +8,26 @@ using namespace std;
 class Solution {
   public:
     string prevPermutation(string s) {
-        bool isPrev = prev_permutation(s.begin(), s.end());
-        if(isPrev)
-            return s;
+        int n = s.length(), k, l;
+        
+        for(k=n-2; k>=0; k--)
+        {
+            if(s[k] > s[k+1])
+                break;
+        }
+        
+        if(k < 0)
+            return "None";
             
-        return "None";
+        for(l=n-1; l>k; l--)
+        {
+            if(s[k] > s[l])
+                break;
+        }
+        
+        swap(s[k], s[l]);
+        reverse(s.begin()+k+1, s.end());
+        return s;
     }
 };
 
