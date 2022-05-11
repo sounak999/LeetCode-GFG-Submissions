aeb47466da65e92b46cb1200a64e84fb25856cc0
Sounak Saha <88291739+sounak999@users.noreply.github.com>
2022-05-11 09:03:31 +0530

    Time: 4 ms (35.46%), Space: 5.9 MB (58.60%) - LeetHub

--- 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.cpp
+++ 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.cpp
@@ -1,23 +1,25 @@
 class Solution {
 public:
-    void dfs(int idx, int length, int n, int &count){
-        ++length;
-        
-        if (length == n){
-           ++count;
-           return;
-        }
-        
-        for (int i = idx ; i < 5 ; i++){
-            dfs(i,length,n,count);
-        }
-    }
-    
     int countVowelStrings(int n) {
-        int count = 0;
-        for (int i = 0 ; i < 5 ; i++)
-            dfs(i,0,n,count);
+        int dp[n][5];
         
-        return count;
-    }
+        // int dp[i][j] => Nummber of strings of length i+1 and ending at character j
+        
+        for (int i = 0 ; i < 5 ; i++)
+            dp[0][i] = 1;
+        
+        for (int i = 1 ; i < n ; i++){
+            dp[i][0] = dp[i-1][0];
+            dp[i][1] = dp[i-1][1] + dp[i-1][0];
+            dp[i][2] = dp[i-1][2] + dp[i-1][1] + dp[i-1][0];
+            dp[i][3] = dp[i-1][3] + dp[i-1][2] + dp[i-1][1] + dp[i-1][0];
+            dp[i][4] = dp[i-1][4] + dp[i-1][3] + dp[i-1][2] + dp[i-1][1] + dp[i-1][0];
+        }
+        
+        int sum = 0;
+        for (int i = 0 ; i < 5 ; i++)
+            sum += dp[n-1][i];
+        
+        return sum;
+    }   
 };
