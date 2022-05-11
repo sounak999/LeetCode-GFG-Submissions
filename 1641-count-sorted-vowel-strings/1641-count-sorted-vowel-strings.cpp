810673c1c9e8c74a5f6f3046fc8f27b2e2c2ffdb
Sounak Saha <88291739+sounak999@users.noreply.github.com>
2022-05-11 09:04:02 +0530

    Time: 4 ms (35.46%), Space: 5.9 MB (85.48%) - LeetHub

--- 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.cpp
+++ 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.cpp
@@ -1,25 +1,6 @@
 class Solution {
 public:
     int countVowelStrings(int n) {
-        int dp[n][5];
-        
-        // int dp[i][j] => Nummber of strings of length i+1 and ending at character j
-        
-        for (int i = 0 ; i < 5 ; i++)
-            dp[0][i] = 1;
-        
-        for (int i = 1 ; i < n ; i++){
-            dp[i][0] = dp[i-1][0];
-            dp[i][1] = dp[i-1][1] + dp[i-1][0];
-            dp[i][2] = dp[i-1][2] + dp[i-1][1] + dp[i-1][0];
-            dp[i][3] = dp[i-1][3] + dp[i-1][2] + dp[i-1][1] + dp[i-1][0];
-            dp[i][4] = dp[i-1][4] + dp[i-1][3] + dp[i-1][2] + dp[i-1][1] + dp[i-1][0];
-        }
-        
-        int sum = 0;
-        for (int i = 0 ; i < 5 ; i++)
-            sum += dp[n-1][i];
-        
-        return sum;
-    }   
+        return (n + 1) * (n + 2) * (n + 3) * (n + 4) / 24;
+    }
 };
