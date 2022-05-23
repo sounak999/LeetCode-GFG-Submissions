02264afe258c3c1cc057490f629dfe8935709798
Sounak Saha <88291739+sounak999@users.noreply.github.com>
2022-05-23 22:32:24 +0530

    Added solution - LeetHub

---
+++ Minimum swaps and K together - GFG/minimum-swaps-and-k-together.cpp
@@ -0,0 +1,71 @@
+// { Driver Code Starts
+// C++ program to find minimum swaps required
+// to club all elements less than or equals
+// to k together
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+ // } Driver Code Ends
+
+
+class Solution
+{
+public:
+    int minSwap(int arr[], int n, int k) {
+        
+        // first count the no. of elements <= k
+        int cnt=0;
+        for(int i=0; i<n; i++)
+        {
+            if(arr[i] <= k)
+                cnt++;
+        }
+        
+        // window size will be cnt and we'll count greater than k
+        int gtr = 0; 
+        for(int i=0; i<cnt; i++)
+        {
+            if(arr[i] > k)
+                gtr++;
+        }
+        
+        // sliding window
+        int res = gtr;
+        for(int i=0, j=cnt; j<n; i++, j++)
+        {
+            if(arr[i] > k)
+                gtr--;
+                
+            if(arr[j] > k)
+                gtr++;
+                
+            res = min(res, gtr);
+        }
+        
+        return res;
+    }
+};
+
+
+// { Driver Code Starts.
+
+// Driver code
+int main() {
+
+	int t,n,k;
+	cin>>t;
+	while(t--)
+    {
+        cin>>n;
+        int arr[n];
+        for(int i=0;i<n;i++)
+            cin>>arr[i];
+        cin>>k;
+        Solution ob;
+        cout << ob.minSwap(arr, n, k) << "\n";
+    }
+	return 0;
+}
+  // } Driver Code Ends
