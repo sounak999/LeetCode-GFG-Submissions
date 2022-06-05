23962718ee244ea4d7bf90d26e354ec80d960a7e
Sounak Saha <88291739+sounak999@users.noreply.github.com>
2022-06-05 12:16:56 +0530

    Time: 31 ms (19.28%), Space: 10.2 MB (18.67%) - LeetHub

--- 52-n-queens-ii/52-n-queens-ii.cpp
+++ 52-n-queens-ii/52-n-queens-ii.cpp
@@ -1,63 +1,34 @@
 class Solution {
-    bool isSafe(int row, int col, vector<vector<int>> &board, int n)
-    {
-        int duprow = row, dupcol = col;
-        // upper left diagonal
-        while(row >= 0 && col >= 0)
-        {
-            if(board[row][col] == 1)
-                return false;
-            
-            row--;
-            col--;
-        }
-        
-        // left 
-        row = duprow, col = dupcol;
-        while(col >= 0)
-        {
-            if(board[row][col] == 1)
-                return false;
-            
-            col--;
-        }
-        
-        // lower left diagonal
-        row = duprow, col = dupcol;
-        while(row < n && col >= 0)
-        {
-            if(board[row][col] == 1)
-                return false;
-            
-            row++;
-            col--;
-        }
-        
-        return true;
-    }
     
-    int solve(int col, vector<vector<int>> &board, int n)
+    int solve(int col, vector<vector<int>> &board, vector<int> leftRow, vector<int> upperDiagonal, vector<int> lowerDiagonal, int n)
     {
         if(col == n)
             return 1;
         
         int cnt = 0;
-        for(int row = 0; row < board.size(); row++)
+        for(int row = 0; row < n; row++)
         {
-            if(isSafe(row, col, board, n))
+            if(!leftRow[row] && !lowerDiagonal[row+col] && !upperDiagonal[n-1 + col-row])
             {
                 board[row][col] = 1;
-                cnt += solve(col+1, board, n);
-                board[row][col] = 0;
+                leftRow[row] = 1;
+                lowerDiagonal[row+col] = 1;
+                upperDiagonal[n-1 + col - row] = 1;
+                cnt += solve(col+1, board, leftRow, upperDiagonal, lowerDiagonal, n);
+                leftRow[row] = 0;
+                lowerDiagonal[row+col] = 0;
+                upperDiagonal[n-1 + col - row] = 0;
             }
         }
         
         return cnt;
     }
+    
 public:
     int totalNQueens(int n) {
         vector<vector<int>> board(n, vector<int>(n, 0));
+        vector<int> leftRow(n, 0), upperDiagonal(2*n-1, 0), lowerDiagonal(2*n-1, 0);
         int col = 0;
-        return solve(0, board, n);
+        return solve(0, board, leftRow, upperDiagonal, lowerDiagonal, n);
     }
 };
