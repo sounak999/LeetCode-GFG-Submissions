8af6e6e50a2291efd6abb901f8b7e14775166330
Sounak Saha <88291739+sounak999@users.noreply.github.com>
2022-06-13 14:38:51 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (68.00%) - LeetHub

--- 216-combination-sum-iii/216-combination-sum-iii.cpp
+++ 216-combination-sum-iii/216-combination-sum-iii.cpp
@@ -1,21 +1,19 @@
 class Solution {
-    void solve(vector<int>& arr, vector<vector<int>>& ans, vector<int>& output, int k, int n, int index)
+    void solve(vector<vector<int>>& ans, vector<int>& output, int k, int n, int num)
     {
-        if(n == 0)
+        if((n == 0) && (k == 0))
         {
-            if(output.size() == k)
-                ans.push_back(output);
-            
+            ans.push_back(output);
             return; 
         }
         
-        if(output.size() > k || n < 0)
+        if(k < 0 || n < 0)
             return ;
         
-        for(int i=index; i<arr.size(); i++)
+        for(int i=num; i<10; i++)
         {
-            output.push_back(arr[i]);
-            solve(arr, ans, output, k, n-arr[i], i+1);
+            output.push_back(i);
+            solve(ans, output, k-1, n-i, i+1);
             output.pop_back();
         }
     }
@@ -23,12 +21,8 @@ public:
     vector<vector<int>> combinationSum3(int k, int n) {
         vector<vector<int>> ans;
         vector<int> output;
-        vector<int> arr;
         
-        for(int i=1; i<=9; i++)
-            arr.push_back(i);
-        
-        solve(arr, ans, output, k, n, 0);
+        solve(ans, output, k, n, 1);
         return ans;
     }
 };
