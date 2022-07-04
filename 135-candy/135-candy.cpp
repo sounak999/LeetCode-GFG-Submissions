c9aa6a8f203d05683033a7192310e9493cf14439
Sounak Saha <88291739+sounak999@users.noreply.github.com>
2022-07-04 15:00:42 +0530

    Time: 19 ms (95.82%), Space: 17.8 MB (40.68%) - LeetHub

--- 135-candy/135-candy.cpp
+++ 135-candy/135-candy.cpp
@@ -2,24 +2,25 @@ class Solution {
 public:
     int candy(vector<int>& ratings) {
         int n = ratings.size();
-        vector<int> left(n, 1);
-        vector<int> right(n, 1);
+        vector<int> aux(n, 1);
         
         for(int i=1; i<n; i++)
         {
             if(ratings[i-1] < ratings[i])
-                left[i] = left[i-1] + 1;
-        }
-        
-        for(int i=n-2; i>=0; i--)
-        {
-            if(ratings[i+1] < ratings[i])
-                right[i] = right[i+1] + 1;
+                aux[i] = aux[i-1] + 1;
         }
         
         int res = 0;
-        for(int i=0; i<n; i++)
-            res += max(left[i], right[i]);
+        for(int i=n-2; i>=0; i--)
+        {
+            if(ratings[i+1] < ratings[i])
+                aux[i] = max(aux[i+1] + 1, aux[i]);
+        }
+        
+        for(auto c: aux)
+        {
+            res += c;
+        }
         
         return res;
     }
