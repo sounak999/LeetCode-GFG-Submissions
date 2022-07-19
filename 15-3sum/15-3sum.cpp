88ddd35272219c53abd7fec20f1eb7cc8fae559f
Sounak Saha <88291739+sounak999@users.noreply.github.com>
2022-07-20 02:48:53 +0530

    Time: 105 ms (78.32%), Space: 19.9 MB (85.94%) - LeetHub

--- 15-3sum/15-3sum.cpp
+++ 15-3sum/15-3sum.cpp
@@ -2,25 +2,38 @@ class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
         vector<vector<int>> res;
-        unordered_map<int, int> mp;
-        
         sort(nums.begin(), nums.end());
         
-        for(int i=0; i<nums.size(); i++) {
-            mp[nums[i]] = i;
-        }
-        
-        for(int i=0; i<nums.size(); i++) {
+        for(int i=0; i<nums.size()-2; i++) 
+        {
             if(i != 0 && nums[i] == nums[i-1])
                 continue;
             
-            for(int j=i+1; j<nums.size(); j++) {
-                if(j != i+1 && nums[j] == nums[j-1])
-                    continue;
-                
-                int rem = -(nums[i] + nums[j]);
-                if(mp[rem] > j)
-                    res.push_back({nums[i], nums[j], rem});
+            int lo = i+1, hi = nums.size()-1;
+            int sum = -nums[i];
+            while(lo < hi)
+            {
+                int curSum = nums[lo] + nums[hi];
+                if(curSum == sum) {
+                    res.push_back({nums[i], nums[lo], nums[hi]});
+                    
+                    while(lo < hi && nums[lo] == nums[lo+1])
+                        lo++;
+                    
+                    while(lo < hi && nums[hi] == nums[hi-1])
+                        hi--;
+                    
+                    lo++;
+                    hi--;
+                } 
+                else if(curSum > sum) 
+                {
+                    hi--;
+                } 
+                else 
+                {
+                    lo++;
+                }
             }
         }
         
