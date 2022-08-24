4b21afca16b7474f0dc12adfab02c7223254e2fc
Sounak Saha <88291739+sounak999@users.noreply.github.com>
2022-08-24 14:50:34 +0530

    Time: 27 ms (73.41%), Space: 6.3 MB (7.72%) - LeetHub

--- 326-power-of-three/326-power-of-three.cpp
+++ 326-power-of-three/326-power-of-three.cpp
@@ -4,9 +4,18 @@ public:
         if(n < 1)
             return false;
         
+        /*
         while(n%3 == 0)
             n /= 3;
         
         return (n == 1);
+        */
+        
+        double x = log10(n) / log10(3);
+        
+        if(x - (int)x == 0)
+            return true;
+        
+        return false;
     }
 };
